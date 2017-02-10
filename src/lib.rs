06984a6cd3854b374ca502bb8a5dfd0b5fb27348
Ram <quadrupleslap@gmail.com>
2017-02-10 20:45:56 +1100

    Added isatty check.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate ansi_term;
-extern crate log;
 extern crate env_logger;
+extern crate libc;
+extern crate log;
 
 use ansi_term::Colour::*;
 use env_logger::LogBuilder;
@@ -13,19 +14,46 @@ pub fn init(envar: &str) {
 
     builder.filter(None, LogLevelFilter::Info);
 
-    builder.format(|record| {
-        format!("{} {}", match record.level() {
-            Error =>    Red.paint("[ERROR]"),
-            Warn  => Yellow.paint(" [WARN]"),
-            Info  =>   Cyan.paint(" [INFO]"),
-            Debug =>  Green.paint("[DEBUG]"),
-            Trace => Purple.paint("[TRACE]"),
-        }, record.args())
-    });
+    if ansi_supported() {
+        builder.format(|record| {
+            format!("{} {}", match record.level() {
+                Error =>    Red.paint("[ERROR]"),
+                Warn  => Yellow.paint(" [WARN]"),
+                Info  =>   Cyan.paint(" [INFO]"),
+                Debug =>  Green.paint("[DEBUG]"),
+                Trace => Purple.paint("[TRACE]"),
+            }, record.args())
+        });
+    } else {
+        builder.format(|record| {
+            format!("{} {}", match record.level() {
+                Error => "[ERROR]",
+                Warn  => " [WARN]",
+                Info  => " [INFO]",
+                Debug => "[DEBUG]",
+                Trace => "[TRACE]",
+            }, record.args())
+        });
+    }
 
     if let Ok(var) = env::var(envar) {
        builder.parse(&var);
     }
 
     builder.init().unwrap();
+}
+
+#[cfg(windows)] 
+fn ansi_supported() -> bool {
+    //TODO: Except Windows 10 "Threshold 2".
+    false
+}
+
+#[cfg(not(windows))]
+fn ansi_supported() -> bool {
+    let isatty = unsafe {
+        libc::isatty(libc::STDOUT_FILENO)
+    };
+
+    isatty != 0
 }
